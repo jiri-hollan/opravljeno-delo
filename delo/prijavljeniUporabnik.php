eb25ebb58da32f3449207eb9a7a3de09dd7816de
Jiri Hollan <jiri.hollan@gmail.com>
2024-12-25 17:24:19 +0100

    premeščen uname

--- delo/prijavljeniUporabnik.php
+++ delo/prijavljeniUporabnik.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
+$uname = !empty($_SESSION["uname"]) ? $_SESSION["uname"] : "";
 require_once '../skupne/sabloni/zahlavi.php';
 require_once 'sabloni/forma.php';
-
-$uname = !empty($_SESSION["uname"]) ? $_SESSION["uname"] : "";
-
 require_once '../skupne/database.php';
 
 	$podminka = array("uname"=>$uname);
