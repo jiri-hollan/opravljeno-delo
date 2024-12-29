8a383fd43b1a0b76ea7b9d06183a80dd031b52d1
Jiri Hollan <jiri.hollan@gmail.com>
2024-12-29 13:47:29 +0100

    šele zdaj je pravilen

--- delo/prijavljeniUporabnikObjekt.php
+++ delo/prijavljeniUporabnikObjekt.php
@@ -9,8 +9,8 @@ require_once('opraviloVsi.php');
 	$podminka = array("uname"=>$uname);
 	///vyberUporabnikaFunction($podminka);
 
-<script src="js/delo.js?'.time().'"></script>	 
-
+     // <script src="js/delo.js?'.time().'"></script>	 
+ echo'<script src="js/delo.js?'.time().'"></script>';	
 //_________________________________
  
  	class Test_input {
