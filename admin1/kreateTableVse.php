95ad025dbbee60f4c3be06249734c89aa4a76db6
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-01 18:17:34 +0100

    Izbris vzorčnih, popravilo bugov

--- admin1/kreateTableVse.php
+++ admin1/kreateTableVse.php
@@ -72,11 +72,10 @@ $definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	`geslo` varchar(255) CHARACTER SET utf8 COLLATE utf8_slovenian_ci NOT NULL,
 	`ime` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
 	`priimek` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-	`stevilkaZdrav` int(11),
+	`stevilkaZdrav` int(3),
     `upstatus` int(3) NOT NULL,
     `pristop` int(3) NOT NULL,	
-	UNIQUE (email, uname),
-	 stevilkaZdravnika  int(3)";	
+	UNIQUE (email, uname)";	
 $databaseGloboka->naredi('uporabnikiTbl', $definice);
 break;
 
