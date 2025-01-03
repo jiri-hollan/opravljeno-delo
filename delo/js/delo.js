167ea0fb77399474a1d5aa601f6f9a5f29feaeab
Jiří Hollan <jiri.hollan@gmail.com>
2025-01-03 14:05:01 +0100

    solidno urejen videz vnosnega formularja

--- delo/js/delo.js
+++ delo/js/delo.js
@@ -20,7 +20,7 @@ switch(akce) {
     stevilkaZdravnika= '<input type="hidden" id="stevilkaZdravnikaId" class="kratke" name="stevilkaZdravnika" value="'+stevilkaZd+'" placeholder="_____" readonly >';
     datumOpravila= '<label for="datumId">datum opravila: </label><br><input type="date" id="datumId" name="datumOpravila" value="" placeholder="datumOpravila" required>';
     sifraOpravila= '<input type="hidden" id="sifraId" class="kratke"  name="sifraOpravila" value="" placeholder="0" readonly required>';
-    opravilo= '<input id="opraviloId" value="" name="opravilo" required autocomplete="off" onchange="zadnjiAlertFunction()">';		
+    opravilo= '<label for="opraviloId">vrsta opravila: </label><br><input id="opraviloId" value="" name="opravilo" required autocomplete="off" onchange="zadnjiAlertFunction()">';		
     casOpravila= '<input type="number" id="casId" class="kratke"  name="casOpravila" min="0" step="1" value="" placeholder="minute" autocomplete="off"  required>';
 	izbira= '<select id="opravilaId"   onchange="myFunction()"><option>opravilo</select>';
     var opraviloList = JSON.parse(opraviloJson);
