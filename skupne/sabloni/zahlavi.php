e839242d68b85a236df58dc85517f11b62331136
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-04 18:00:59 +0100

    spremenjeno zahlavi

--- skupne/sabloni/zahlavi.php
+++ skupne/sabloni/zahlavi.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../skupne/css/zahlavi.css?<?php echo time(); ?>">
+<link rel="stylesheet" href="../servis/css/pregledovalci.css?<?php echo time(); ?>">
 <title>Anestiz</title>
 <link rel="shortcut icon" href="../favicon.ico?<?php echo time(); ?>">
 </head>
