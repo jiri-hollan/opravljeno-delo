9096fac7c08822d43d8282554b6cda1e494fd828
Jiří Hollan <jiri.hollan@gmail.com>
2025-01-06 14:46:21 +0100

    Dodan if stevilka zdravnika

--- delo/identifikace.php
+++ delo/identifikace.php
@@ -18,18 +18,26 @@ $stevilkaZdravnika=($vybrano[0]["stevilkaZdravnika"]);
 $ime=($vybrano[0]["ime"]);	
 $priimek=($vybrano[0]["priimek"]);
 $bolnisnica=($vybrano[0]["bolnisnica"]);	
-$identifikace=' '.$stevilkaZdravnika.' '.$ime.' '.$priimek.' '.$bolnisnica;
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+if($stevilkaZdravnika>0){
+	$identifikace=' '.$stevilkaZdravnika.' '.$ime.' '.$priimek.' '.$bolnisnica;
 $GLOBALS['stevilkaZdravnika']=$stevilkaZdravnika;
 $GLOBALS['identifikace']=$identifikace;
 //echo $GLOBALS['identifikace'];
-}//od if(cout)
-	else{
-   echo "Za izbranega uporabnika ni zapisa v bazi";	
-}//od else
 echo'<script>
 identifikace="'.$GLOBALS['identifikace'].'";
 identifikaceFunction(identifikace);
 </script>';
+	}else{
+		   echo "V bazi ni vaše zdravniške številke";	
+	}
+//..........................................................................................................
+
+}//od if(cout)
+	else{
+   echo "Za izbranega uporabnika ni zapisa v bazi";	
+}//od else
+
 	}//od construct
 		}//od class vyber uporabnika
 ?>
