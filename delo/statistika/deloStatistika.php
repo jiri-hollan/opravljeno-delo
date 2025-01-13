8b811390a28049fd780b9fe12a87b42e238529a0
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-13 17:49:56 +0100

    načrtovanje statistike

---
+++ delo/statistika/deloStatistika.php
@@ -0,0 +1,13 @@
+<?php
+/**
+*V prvem bloku pobere iz uporabnikiTbl vse zapise v katerih je vnesena številka zdravnika
+*in prikaže ime, priimek, številka zdravnika, upstatus in gdpr.
+*s klikom na določenega zdravnika se odpre menu, kje se lahko: 
+*1.Izbere število ur v določenem dnevu ali v dnevih od do. 
+*2.Lahko se pogleda, kaj vse je bilo v določenem dnevu (dnevih) vpisano.
+*3.Lahko se pogleda po šifri oravila trajanje le tega po dnedih ali v odstotku
+*
+*
+*
+**/
+?>
