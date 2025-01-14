0bbfd653683850bb5223f7561dc938a3a5a383c2
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-14 16:15:03 +0100

    zapisano zaporedje array iz  uporabnikiTbl

--- admin1/manipulaceUporabniki.php
+++ admin1/manipulaceUporabniki.php
@@ -87,7 +87,7 @@ case "odstrani":
 
 function vyberFunction($podminka){
    $tabulka="uporabnikiTbl";
-   $stolpci=["*"];
+   $stolpci=["id", "email", "uname", "geslo", "bolnisnica", "ime", "priimek", "stevilkaZdravnika", "upstatus", "pristop", "gdpr"];
    $vyber = new database();
    $vybrano=$vyber->vyber($tabulka, $stolpci, $podminka );
 //echo $vybrano[1];
