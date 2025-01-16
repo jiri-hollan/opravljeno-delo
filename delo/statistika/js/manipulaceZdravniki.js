c17aa63e76a8857703d5deec7d622f08f1ac51fa
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-16 18:58:38 +0100

    zdaj nekako izbere številko Zdravnika

--- delo/statistika/js/manipulaceZdravniki.js
+++ delo/statistika/js/manipulaceZdravniki.js
@@ -1,7 +1,7 @@
 function izberiStevilkoZdravnikaFunction(akce) {
   document.getElementById("akceId").value = akce;
 switch(akce) {
-  case "vyber":
+ /* case "nnn":
     document.getElementById("demo").innerHTML = '<input type="text" id="priimekId" name="priimek" value="" placeholder="Priimek">';// omogoči izbiro po priimku
 	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit">'; //to je submit
     break; 
@@ -9,8 +9,8 @@ switch(akce) {
   case "vloz":
    alert("ni omogočeno");
     break;
-
-  case "uredi":
+*/
+  case "vyber":
   if(document.getElementById("osebe")!=null){
  document.getElementById("osebe").addEventListener("click", functionOver);
 }
@@ -34,7 +34,7 @@ function functionOver (e) {
   var x = e.target;
   if (x.nodeName == "TD") {
    var y = event.composedPath()[1];
-   row_value = y.cells[0].innerHTML;
+   row_value = y.cells[4].innerHTML;
    document.getElementById("demo3").innerHTML = "id v bazi je= " + row_value ;  
  }//od if 
  window.location.href = "manipulaceZdravniki.php?akce=" + x.innerHTML + "&id=" + row_value;  
