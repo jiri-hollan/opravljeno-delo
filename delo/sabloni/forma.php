6f6fe5c1e46f6bfad3d3ae1b41c5981cc774fff9
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-16 16:30:35 +0100

    popravljen bug pri vloženem PHP

--- delo/sabloni/forma.php
+++ delo/sabloni/forma.php
@@ -1,7 +1,11 @@
-<link rel="stylesheet" href="css/delo.css?<?php echo time(); ?>">
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<?php
+echo'
+<link rel="stylesheet" href="css/delo.css?'.time().'">
+<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">
 <input type="hidden" id="akceId" name="akce" value="">
 <p id="demo"></p>
 <p id="posli"></p>
 </form>
 <p id="demo3"></p>
+';
+?>
