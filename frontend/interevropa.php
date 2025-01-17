ed9567015e712b80ead596c9a0c8524c0bed0289
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-17 18:03:28 +0100

    interevropa

---
+++ frontend/interevropa.php
@@ -0,0 +1,10 @@
+<?php
+require_once('administrace.php');
+echo'
+
+<li><a href="https://docs.google.com/spreadsheets/d/1kfkIEJ4aj7FlxVslJgg6EyAGyhfAzSkDnNJkkj-BvWk/edit#gid=0">2023</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1EOfMCWwT0CTMXpp9Cg94E9pXdg08CTJELzl1xSLs2V8/edit?gid=1081304001#gid=1081304001">2024</a></li>
+<li><a href=" https://docs.google.com/spreadsheets/d/17Vc7Y8tI8u99gPTqdLqpsTUTsr5fEwBpKlSMiqA7HME/edit?usp=sharin">2025</a></li>
+';
+
+?>
