5b3df8253cdd73175247c594b04fd3bf111e27bc
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-18 10:46:12 +0100

    zakomentirano v menu

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -3,10 +3,10 @@
       <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
       <li><span onclick=sbFunction("spomin")>Pregled</span>  </li>
 	  <li><a href="../frontend/interevropa.php">INTEREVROPA</a> </li>
-      <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>'; 
-	 $a1= '<li><a href="deloMenu.php">delo</a> </li>';
+      <!--<li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>-->'; 
+	 $a1= '<!--<li><a href="deloMenu.php">delo</a> </li>-->';
 	 $a2= '<li><a href="../servis/menuUpload.php">servis</a> </li>
 	       <li><a href="../servis/nastavitveMenu.php">nastavitve</a> </li>'; 
 	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li> 
-	       <li><a href="../delo/statistika/statistikaMenu.php">statistika</a> </li>'; 
+	       <!--<li><a href="../delo/statistika/statistikaMenu.php">statistika</a> </li>-->'; 
 ?>
