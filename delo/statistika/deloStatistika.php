54ceab012de02934c92df084eb5f24dce0e73b2c
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-19 13:05:36 +0100

    vse prekopirano iz manipulaceZdravniki.php

--- delo/statistika/deloStatistika.php
+++ delo/statistika/deloStatistika.php
@@ -49,5 +49,66 @@ GROUP BY datumOpravila;
 ';
 
 **/
+//CCCCCCCCCCCCCCC CLASS VYBER IMA STEVIKLO CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+class VyberImaStevilko {
+public $tabulka;
+ function __construct( $podminka) {
+	    $tabulka="uporabnikiTbl";
+   /* stolpci se morajo ujemati z nadpisi stlpcev v "if(count)" linija 105*/
+   $stolpci=["id", "bolnisnica", "ime", "priimek", "stevilkaZdravnika"];
+   $vyber = new database();
+   $vybrano=$vyber->vyberPogoj($tabulka, $stolpci, $podminka );
+//echo $vybrano[1];
+//echo var_dump($vybrano);
+ //  echo "<br>";
+ echo count($vybrano);
+//$dolzina=count($vybrano);
+//echo $vybrano[1];
+//echo "<br>";
+  if(count($vybrano)>0){
+  echo "<table id='osebe' style='border: solid 1px black;'>";
+/* nadpisi se morajo ujemati s prikazanimi stlpci v vyberFunction*/
+  echo "<tr class='glavaTable'><th>Id</th><th>bolnisnica</th><th>ime</th><th>priimek</th><th>stevilkaZdravnika</th></tr>";
+    foreach(new TableRows(new RecursiveArrayIterator($vybrano)) as $k=>$v) {
+        echo $v;
+   }//od foreach
+  }//od if(cout) 
+ }//od construct  
+}//od class VyberImaStevilko
+//CCCCCCCCCCCCC KONEC  CLASS VYBER IMA STEVIKLO CCCCCCCCCCCCCCCCCCCCCCCCCCC
 
+
+
+
+//CCCCCCCCCCCCCCC CLASS TABLE ROWS CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+    function current() { 
+		 return "<td  >"  . parent::current() . "</td>";
+    }
+    function beginChildren() {
+        echo "<tr>";
+    }
+    function endChildren() {
+        echo "<td onclick=" . '"izberiStevilkoZdravnikaFunction('. "'vyber'".')"'.'"' . ">izberi</td></tr>" . "\n";
+    }//od endChildren
+}// od class TableRows
+//CCCCCCCCCCCCCCC KONEC CLASS TABLE ROWS CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+
+$stevilkaZdravnika="1"; //"= 1"  TO DOLOČI, DA SO ZBRANI LE ZAPISI S ŠTEVILKO >0
+
+if ($stevilkaZdravnika >0) {
+$podminka = array("stevilkaZdravnika>"=>0);	   
+//$podminka = array("stevilkaZdravnika>"=>$stevilkaZdravnika);
+} else {
+	   $podminka = NULL;
+       }
+new VyberImaStevilko($podminka);
+echo'
+<script src="js/manipulaceZdravniki.js?'.time().'">
+</script>';
+require_once 'sabloni/prijavljenJe.php';
+require_once '../../skupne/sabloni/zapati.php';
 ?>
