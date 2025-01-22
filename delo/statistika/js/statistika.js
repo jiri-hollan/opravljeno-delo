9fb1534bdc81f236cc77d47bf5c951d87a778ec0
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-22 10:10:16 +0100

    prekopirana pastana  izbor zdaj pogojFunction

--- delo/statistika/js/statistika.js
+++ delo/statistika/js/statistika.js
@@ -14,7 +14,58 @@ let i;
 document.getElementById("opravilaId").innerHTML = text;
 }
 
-function pogojiFunction(akce,stevilkaZd) {
+function pogojFunction(akce,stevilkaZd) {
+
+  document.getElementById("akceId").value = akce;
+switch(akce) {
+  case "vyber":
+    document.getElementById("demo").innerHTML = '<input type="text" id="bolnisnicaId" name="bolnisnica" value="" placeholder="Bolnišnica">';// omogoči izbiro bolnišnice
+	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit">'; //submit
+    break; 
+
+  case "vloz":
+   let stevilkaZdravnika= '<input type="hidden" id="stevilkaZdravnikaId" class="kratke" name="stevilkaZdravnika" value="'+stevilkaZd+'" placeholder="_____" readonly >';
+   let datumOpravila= '<label for="datumId">datum opravila: </label><br><input type="date" id="datumId" name="datumOpravila" value="" placeholder="datumOpravila" required>';
+   let sifraOpravila= '<input type="hidden" id="sifraId" class="kratke"  name="sifraOpravila" value="" placeholder="0" readonly required>';
+   let opravilo= '<label for="opraviloId">vrsta opravila: </label><br><input id="opraviloId" value="" name="opravilo" required autocomplete="off" onchange="zadnjiAlertFunction()">';		
+   let casOpravila= '<input type="number" id="casId" class="kratke"  name="casOpravila" min="0" step="1" value="" placeholder="minute" autocomplete="off"  required>';
+   let izbira= '<select id="opravilaId"   onchange="myFunction()"><option>opravilo</select>';
+    const opraviloList = JSON.parse(opraviloJson);
 	
+//alert("opravilo Json:" + opraviloJson);
+//alert(opraviloList);
+  
+    document.getElementById("demo").innerHTML = "<div class='glavni'>"+stevilkaZdravnika + datumOpravila + sifraOpravila +"<div class='notranji'>"+ opravilo + casOpravila +"<br>"+ izbira +"</div></div>" ;
+	   // document.getElementById("klik").innerHTML =	izbira;
+	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit"><input type="reset" name="reset" value="Reset">'; //submit+reset
 	
+	listaOpravilFunction(opraviloList);
+	
+    break;
+
+  case "uredi":
+  //alert("v JS case edit");
+  if(document.getElementById("osebe")!=null){
+ document.getElementById("osebe").addEventListener("click", functionOver);
 }
+
+    break;
+
+  case "odstrani":
+    if ( confirm("v funkciji JS odstrani\odstranim en zapis?") == true) {
+    if(document.getElementById("osebe")!=null){
+    document.getElementById("osebe").addEventListener("click", functionOver);
+      }
+} else {
+  //alert( "You canceled!");
+}
+  /* if(document.getElementById("osebe")!=null){
+ document.getElementById("osebe").addEventListener("click", functionOver);
+ }*/
+   // code block
+
+    break;	
+  default:
+    // code block
+ }//od switch
+} // od izborFunction
