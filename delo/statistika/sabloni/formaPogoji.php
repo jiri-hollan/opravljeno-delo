dfdc5d13e4edb0dbd949847730f4a2016f5c21e4
Jiří Hollan <jiri.hollan@gmail.com>
2025-01-22 12:21:24 +0100

    Odstranjena class
    dodan opraviloVsiS s to class

--- delo/statistika/sabloni/formaPogoji.php
+++ delo/statistika/sabloni/formaPogoji.php
@@ -33,40 +33,4 @@ echo'
 <p id="demo3"></p>
 <script>pogojFunction("vloz","6027");</script>
 ';
-
-Class PoberZapis{
-	public $conn;
-	public $pristop;
-	public function __construct($bolnisnica) {
- $this->conn = new Database();	
- $this->nameTable = 'opravilaTbl';
- $stolpci = array('sifraOpravila','opravilo');
- $poradi = "";
-//bolnisnicapregledId je obsoječa bolnisnica v tabeli pregledovalciKomb
- $podminka = array(""); 
- $prebrano = $this->conn->vyber($this->nameTable, $stolpci, $podminka, $poradi);  
-//var_dump($prebrano); 
- $opravilo=[];
- for ($i = 0; $i < count($prebrano); $i++) {
-//echo $prebrano[$i]["opravilo"].'<br>';	
- //$opravilo= $prebrano[$i]["sifraOpravila"].':'.$prebrano[$i]["opravilo"];
- $sifra=$prebrano[$i]["sifraOpravila"];
- //echo'šifra= '.$sifra;
- $oprav=$prebrano[$i]["opravilo"];
-  $opravilo[$sifra]=$oprav;
-//echo '<br>'.$opravilo.'<br>';//izpiše  opravilo na zaslon
-echo'<br>';
-//var_dump ($opravilo);
-
-}//od for 
-//echo'<br>';
-//var_dump($opravilo);
-//echo'<br>';
-  $opraviloJson = json_encode($opravilo, JSON_UNESCAPED_UNICODE);
-// echo $opraviloJson;
-  echo '<script>';
-  echo 'var opraviloJson= ' . json_encode( $opraviloJson, JSON_UNESCAPED_UNICODE) . ';';
-  echo '</script>';
-}//od construct	
-}//od class PoberZapis
 ?>
