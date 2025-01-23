a9eccded9e2d510777ef943fc03980a8f32f4b75
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-23 18:04:08 +0100

    preprečitev cache v razpisMeseci.js

--- oddelek/js/razpisMeseci.js
+++ oddelek/js/razpisMeseci.js
@@ -1,8 +1,9 @@
 //izbira meseca 
 function myFunction(a, b) {
+	c = Math.floor(Math.random() * 100);
   switch(b) {
 	case "dez":
-    document.getElementById("slika").innerHTML = '<iframe id="tabela"  name="plugin" src=" ' + 'dezurstva/mesPdf/' + a + '.pdf ">' +  '</iframe> ' ;
+    document.getElementById("slika").innerHTML = '<iframe id="tabela"  name="plugin" src=" ' + 'dezurstva/mesPdf/' + a + '.pdf?'+c+'">' +  '</iframe> ' ;
     break;
   case "raz":
     document.getElementById("slika").innerHTML = '<iframe id="tabela"  name="plugin" src=" ' + 'razpis/mesPdf/' + a + '.pdf ">' +  '</iframe> ' ;
