b46705c91f122098e6b8d73b25b3b6478a05a2b0
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-26 18:00:38 +0100

    tabulka undefined

--- delo/statistika/js/ogledStatistika.js
+++ delo/statistika/js/ogledStatistika.js
@@ -61,7 +61,7 @@ row_value = y.cells[0].innerHTML;
 alert(row_value);
   document.getElementById("demo3").innerHTML = "id v bazi je= " + row_value ;  
  }//od if 
- alert( x.innerHTML + "&id=" + row_value + "&tabulka="+ tabulka );
+ alert( "../ogledObjektPrijavljeni.php?akce=" + x.innerHTML + "&id=" + row_value + "&tabulka="+ tabulka );
   window.location.href = "../ogledObjektPrijavljeni.php?akce=" + x.innerHTML + "&id=" + row_value + "&tabulka="+ tabulka; 
 }//od function(e)
 } // od izborFunction
