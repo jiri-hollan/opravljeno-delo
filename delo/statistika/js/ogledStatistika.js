784320c46eb16d80a5ffca99d0e987d8b37a75fa
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-26 17:55:08 +0100

    dela edit

--- delo/statistika/js/ogledStatistika.js
+++ delo/statistika/js/ogledStatistika.js
@@ -33,7 +33,7 @@ document.getElementById("demo").innerHTML = '<input id="datumVpisaId" type="date
   case "edit":
 //alert("v JS case edit");
    if(document.getElementById("osebe")!=null){
-     document.getElementById("osebe").addEventListener("click", functionOver);
+     document.getElementById("osebe").addEventListener("click", functionOvce);
 }
     break;
 
@@ -61,5 +61,6 @@ row_value = y.cells[0].innerHTML;
 alert(row_value);
   document.getElementById("demo3").innerHTML = "id v bazi je= " + row_value ;  
  }//od if 
+ alert("../ogledObjektPrijavljeni.php?akce=" + x.innerHTML + "&id=" + row_value + "&tabulka="+ tabulka);
   window.location.href = "../ogledObjektPrijavljeni.php?akce=" + x.innerHTML + "&id=" + row_value + "&tabulka="+ tabulka; 
 }//od function(e)
