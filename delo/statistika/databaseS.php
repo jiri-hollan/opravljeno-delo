da41ad48f261431e5a954b43ebc863120b117836
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-28 19:16:21 +0100

    dodana suma

--- delo/statistika/databaseS.php
+++ delo/statistika/databaseS.php
@@ -218,4 +218,47 @@ $dotaz = $this->conn->prepare("SELECT  $sloupceSQL  FROM $tabulka". $podminkaSQL
 	  return $zaznamy;
 	}
 //............konec sumaSumarum............................................................	
+
+//....................funkcija suma v razvoju.........................................
+public function suma($tabulka, $sloupce, $podminka = NULL){
+	$sloupceSQL = implode(', ', $sloupce);
+	//echo '<br>$sloupceSQL= ';
+	//var_dump($sloupceSQL);
+	$podminkaSQL = '';
+	$parametry = array();
+
+	if (is_array($podminka)){
+		$i = 0;
+		foreach ($podminka as $sloupec=>$hodnota){
+			if ($i == 0){
+				$podminkaSQL .=" WHERE $sloupec = ?";				
+			}else {
+				$podminkaSQL .=" AND $sloupec = ?";
+			}
+			$parametry[$i] = $hodnota;
+			$i++;
+		}
+	}
+
+	/*echo '<br>parametry= ';
+	var_dump($parametry);
+	 echo "<br>podminka= ";
+	 var_dump($podminka);
+	echo "<br>podminka SQL: ";
+	var_dump($podminkaSQL );*/
+	$dotaz = $this->conn->prepare("SELECT SUM($sloupceSQL)FROM $tabulka". $podminkaSQL);
+	//var_dump($dotaz);
+	try {
+		$dotaz->execute($parametry);		
+		$zaznamy = $dotaz->fetchAll(PDO::FETCH_ASSOC);
+		//echo '<br>v try vyber';
+	  }catch (PDException $e) {
+		  echo $e->getMessage();
+		  $zaznamy = false;
+	  }
+	  
+	  $dotaz->closeCursor();
+	  return $zaznamy;
+	}
+//............konec sum............................................................
 }//uzavírací zavorky class DatabaseS
