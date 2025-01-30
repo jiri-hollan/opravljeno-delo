90f7a47f2e9099d432f7ebbe74367aec1eba2c8c
Jiří Hollan <jiri.hollan@gmail.com>
2025-01-30 14:40:16 +0100

    identifikace.php do statistika

---
+++ delo/statistika/identifikace.php
@@ -0,0 +1,46 @@
+<?php
+class VyberUporabnika {
+public $podminka;
+function __construct($podminka="") {
+	    $this->podminka=$podminka;
+//echo('<br>VyberUporabnika podminka= ');		
+//var_dump($podminka);
+	    $this->tabulka="uporabnikiTbl";
+	    $this->stolpci=["stevilkaZdravnika","ime","priimek","bolnisnica"];
+//echo('<br>VyberUporabnika stolpci= ');
+//var_dump($this->stolpci);
+	    $this->vyber=new databaseS();
+	    $vybrano=$this->vyber->vyber($this->tabulka, $this->stolpci, $this->podminka );
+//echo'<br>VyberUporabnika $vybrano= '.($vybrano[0]["stevilkaZdravnika"]);		
+if(count($vybrano)>0){
+//echo($vybrano[0]["stevilkaZdravnika"]);	
+$stevilkaZdravnika=($vybrano[0]["stevilkaZdravnika"]);
+$ime=($vybrano[0]["ime"]);	
+$priimek=($vybrano[0]["priimek"]);
+$bolnisnica=($vybrano[0]["bolnisnica"]);	
+//----------------------------------
+if($stevilkaZdravnika>0){
+	$identifikace=' '.$stevilkaZdravnika.' '.$ime.' '.$priimek.' '.$bolnisnica;
+$this->stevilkaZdravnika=$stevilkaZdravnika;
+$this->identifikace=$identifikace;
+//echo $this->identifikace;
+echo'<script>
+identifikace="'.$this->identifikace.'";
+identifikaceFunction(identifikace);
+</script>';
+	}else{
+		   echo "V bazi ni vaše zdravniške številke";
+		   echo "<script>
+		   alert('V bazi ni vaše zdravniške številke');		   
+		  window.location.replace('../frontend/deloMenu.php');
+		  </script>";
+	}
+
+}//od if(cout)
+	else{
+   echo "Za izbranega uporabnika ni zapisa v bazi";	
+}//od else
+
+	}//od construct
+		}//od class vyber uporabnika
+?>
