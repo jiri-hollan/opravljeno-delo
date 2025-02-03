c505258a72fdbfc5e6fc4a99012824e9c4cfa4f4
Jiří Hollan <jiri.hollan@gmail.com>
2025-02-03 16:31:18 +0100

    Statistika je zdaj nivo 2

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -6,7 +6,8 @@
 	 $a1= '<li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li> 
 	       <li><a href="deloMenu.php">delo</a> </li>';
 	 $a2= '<li><a href="../servis/menuUpload.php">servis</a> </li>
-	       <li><a href="../servis/nastavitveMenu.php">nastavitve</a> </li>'; 
+	       <li><a href="../servis/nastavitveMenu.php">nastavitve</a> </li>
+		   <li><a href="../delo/statistika/statistikaMenu.php">statistika</a> </li>'; 
 	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li> 
-	       <li><a href="../delo/statistika/statistikaMenu.php">statistika</a> </li>'; 
+'; 
 ?>
