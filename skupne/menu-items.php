156157043fad1c0dbe0a8cacf47a463117eaaeb8
Jiri Hollan <jiri.hollan@gmail.com>
2025-02-11 16:17:50 +0100

    nepomembno

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -8,6 +8,5 @@
 	 $a2= '<li><a href="../servis/menuUpload.php">servis</a> </li>
 	       <li><a href="../servis/nastavitveMenu.php">nastavitve</a> </li>
 		   <li><a href="../delo/statistika/statistikaMenu.php">statistika</a> </li>'; 
-	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li> 
-'; 
+	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li> '; 
 ?>
