653aa727defa212795833806ad5eee4d9a4ca501
Jiri Hollan <jiri.hollan@gmail.com>
2025-02-14 16:15:01 +0100

    sporočilo ob izjemi

--- statistika/deloStatistika.php
+++ statistika/deloStatistika.php
@@ -11,6 +11,9 @@ require_once ('ogledStatistika.php');
 echo'<script src="js/statistika.js?'.time().'"></script>';
 echo'<script src="js/poDatumu.js?'.time().'"></script>';
 echo'<script src="js/ogledStatistika.js?'.time().'"></script>';
+if(isset($_REQUEST['semafor'])){
+new Podminka();
+}else{echo"Nekaj je narobe, obvestite admina!";}
 /**
 *V prvem bloku pobere iz uporabnikiTbl vse zapise v katerih je vnesena številka zdravnika
 *in prikaže ime, priimek, številka zdravnika, upstatus in gdpr.
@@ -173,13 +176,13 @@ new Vyber($this_stevilkaZdravnika,$this_datumOpravila,'deloTbl');
 break;
 
 default:
-echo'linija167';	 
+echo'linija 179';	 
  	 }//od switch semafor	 
  }//od construct  
 }//od class Podminka
 //CCCCCCCCCCCCCCCCCCCC KONEC CLASS PODMINKA CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 
-new Podminka();
+
 echo'<script src="js/manipulaceZdravniki.js?'.time().'"></script>';
 require_once 'sabloni/prijavljenJe.php';
 require_once '../skupne/sabloni/zapati.php';
