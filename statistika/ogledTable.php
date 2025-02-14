adc3b7bcfb0e79dcc922356b7a3972ff848fea5c
Jiří Hollan <jiri.hollan@gmail.com>
2025-02-14 14:50:24 +0100

    pravilen "$nazaj"

--- statistika/ogledTable.php
+++ statistika/ogledTable.php
@@ -1,6 +1,7 @@
 <?php 
  @session_start();
 $uname = !empty($_SESSION["uname"]) ? $_SESSION["uname"] : "";
+$nazaj="statistikaMenu.php";
  require_once('sabloni/zahlavi.php');
 /* V tom failu so funkcije za spreminjanje tabele databaze*/
  require_once('sabloni/formDelo.php');//komentar: gumbi "izberi vlož"
