741dbc93a0bce51b266f257e2c640eb1b524e872
Jiri Hollan <jiri.hollan@gmail.com>
2025-02-16 10:17:17 +0100

    priprava an statistiko pregleda

---
+++ statistika/statistikaPregled.php
@@ -0,0 +1,9 @@
+<?php
+/************************************************************
+*Tu pridejo predlogi SQL za pregled polj v tabli bolnikTbl
+*naj bi vsbovali datum od-do ali določeno leto mesec ...
+*iskanje po pogojih ==,>=,<=,vsebuje
+*kombinacijo pogojev 
+*prikaz pozdravniku
+*grafične prikaze določi dodatna obdelava arrayev
+************************************************************/
