ff307146a9cc6317fe604bbd1b4346b9967beae9
Jiří Hollan <jiri.hollan@gmail.com>
2025-03-06 14:41:13 +0100

    pozicije pogojev s statistiki

--- statistika/sabloni/forma.php
+++ statistika/sabloni/forma.php
@@ -2,8 +2,10 @@
 echo'
 <form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">
 <input type="hidden" id="akceId" name="akce" value="">
+<div class="pogojiDiv">
 <p id="pogojId"></p>
 <p id="posli"></p>
+</div>
 </form>
 <p id="demo3"></p>
 ';
