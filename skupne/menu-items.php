b237e640dc5794db38f93203e23545525c935689
Jiri Hollan <jiri.hollan@gmail.com>
2025-03-15 14:13:24 +0100

    kompetence za statistiko

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -4,9 +4,11 @@
       <li><span onclick=sbFunction("spomin")>Pregled</span>  </li>
 	  <li><a href="../frontend/interevropa.php">INTEREVROPA</a> </li>'; 
 	 $a1= '<li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li> 
-	       <li><a href="deloMenu.php">delo</a> </li>';
+	       <li><a href="deloMenu.php">delo</a> </li>
+		   	<li><a href="../statistika/statistikaMenu.php">statistika</a> </li>
+		   ';
 	 $a2= '<li><a href="../servis/menuUpload.php">servis</a> </li>
 	       <li><a href="../servis/nastavitveMenu.php">nastavitve</a> </li>
-		   <li><a href="../statistika/statistikaMenu.php">statistika</a> </li>'; 
+            '; 
 	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li> '; 
 ?>
--- statistika/menuStatistika-items.php
+++ statistika/menuStatistika-items.php
@@ -1,13 +1,9 @@
 <?php
 $nazaj="statistikaMenu.php";
 $a0= '';
-$a1= '';
-/*'<li><ahref="../delo/prijavljeniUporabnikObjekt.php?nazaj='.$nazaj.'&akce=novZapis">Vpis</a> </li>		   
-<li><a href="../delo/ogledTable.php?tabulka=deloTbl">ogled</a></li>';
-<li><a href="../delo/prijavljeniUporabnikObjekt.php?nazaj='.$nazaj.'&akce=dnevni">Dnevni</a> </li>	   
-<li><a href="../servis/manipulaceObjektUniverzal.php?tabulka=pregledovalciTbl">upravljanje z pregledovalci</a></li>';*/   
+$a1= '<li><a href="pregledStatistika.php?semafor=pregledovalec">pregledi po zdravnikih</a></li>
+';
 $a2= '<li><a href="zdravniki.php?nazaj='.$nazaj.'">delo Zdravnika</a></li>
-<li><a href="pregledStatistika.php?semafor=pregledovalec">pregledi po zdravnikih</a></li>
 <li><a href="pregledStatistika.php?semafor=asa">ASA</a></li>
 <li><a href="pregledStatistika.php?semafor=mallampati">mallampati</a></li>
 <li><a href="pregledStatistika.php?semafor=sklep">sklep</a></li>
