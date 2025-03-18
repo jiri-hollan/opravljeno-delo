19230ae92fc30393ae8e21d001c040155177332b
Jiri Hollan <jiri.hollan@gmail.com>
2025-03-18 17:51:24 +0100

    naslovi stlpcev pravilno

--- admin1/manipulaceUporabniki.php
+++ admin1/manipulaceUporabniki.php
@@ -101,7 +101,7 @@ if(count($vybrano)>0){
   echo'<P><b>pristop:</b>1=na seznamu 2=vlaganje v bazo 3=pogled v bazo</P>';
   echo'<P><b>GDPR:</b>0=prevzame nastavitev programa(0 ali 1) 1=baza vedno dostopna 2=baza nedostopna</P>';
   echo "<table id='osebe' style='border: solid 1px black;'>";
-  echo "<tr class='glavaTable'><th>Id</th><th>e-mail</><th>uporabnik</th><th>geslo</th><th>bolnisnica</th><th>ime</th><th>priimek</th><th>stevilkaZdravnika</th><th>pristop</th><th>GDPR</th><th>upstatus</th></tr>";
+  echo "<tr class='glavaTable'><th>Id</th><th>e-mail</><th>uporabnik</th><th>geslo</th><th>bolnisnica</th><th>ime</th><th>priimek</th><th>stevilkaZdravnika</th><th>upstatus</th><th>pristop</th><th>GDPR</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
