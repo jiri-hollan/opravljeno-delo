4444aa1f9bd17335b2424c86a9b28e56dd7d9291
Jiri Hollan <jiri.hollan@gmail.com>
2025-03-29 17:49:39 +0100

    narejena forma

--- pregled/otroskaPremedikacija.php
+++ pregled/otroskaPremedikacija.php
@@ -1,4 +1,5 @@
 <?php
 require_once '../skupne/database.php';
+require_once 'sabloni/formaOtroskaPremedikacija.php';
 
 ?>
--- pregled/sabloni/formaOtroskaPremedikacija.php
+++ pregled/sabloni/formaOtroskaPremedikacija.php
@@ -1,3 +1,11 @@
 <?php
-
+echo"
+<form id='otroskaFrm' name='otroskaForma' method='post' action='otroskaPremedikacija.php' autocomplete='off'> 
+  <input type='radio' id='midazolamId' name='ucinkovina' value='midazolam'>
+ <label for='midazolamId'>midazolam</label><br>
+  <input type='radio' id='medetomidinId' name='ucinkovina' value='medetomidin'>
+ <label for='medetomidinId'>medetomidin</label><br>
+<input type='submit' value='izberi' name='submit'>
+</form>
+";
 ?>
