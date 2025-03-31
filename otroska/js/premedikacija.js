9df7b52c5a708edfe968f493af9a43adb9b4fcf6
Jiri Hollan <jiri.hollan@gmail.com>
2025-03-31 20:19:43 +0200

    prenese težo

--- otroska/js/premedikacija.js
+++ otroska/js/premedikacija.js
@@ -1,3 +1,19 @@
-function testna(){
-	alert("testna");
+function modalna(a=''){
+	alert("modalna "+a+" kg");
 }
+
+
+
+
+function schovej(a){
+//alert (a);
+document.getElementById(a).style.display='block';
+// Get the modal
+var modal = document.getElementById(a);
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}//od window.onclick
+}//od function schovej
