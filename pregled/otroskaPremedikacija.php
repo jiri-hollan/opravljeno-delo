5a28e50529044e468ef676c63d5b1e7d3ae2d912
Jiří Hollan <jiri.hollan@gmail.com>
2025-03-31 13:56:32 +0200

    prevzame vrednost teza
    poskusne datoteke

--- pregled/otroskaPremedikacija.php
+++ pregled/otroskaPremedikacija.php
@@ -1,9 +1,8 @@
+<input id="teza" type="hidden" value="6"></input>
 <?php
 require_once '../skupne/database.php';
 require_once 'sabloni/formaOtroskaPremedikacija.php';
-?>
-<input id="teza" type="hidden" value="4"></input>
-<?php
+
 if(isset($POST_ucinkovina)&&isset($POST_teza)){
     new Premedikace($POST_ucinkovina, $POST_teza);
 }//else{echo'Ni določena učinkovina ali teža';}
--- pregled/sabloni/formaOtroskaPremedikacija.php
+++ pregled/sabloni/formaOtroskaPremedikacija.php
@@ -1,22 +1,25 @@
+<script>
+
+function otroskaFunction(){
+	   let teza = document. getElementById('teza').value;
+		//alert(teza);
+		//document.getElementById('tezaPremedikacijaId').setAttribute("value",teza);
+		document.getElementById('tezaPremedikacijaId').value=teza;		
+}
+</script>
 <?php
+
 echo"
 <form id='otroskaFrm' name='otroskaForma' method='post' action='otroskaPremedikacija.php' autocomplete='off'> 
   <input type='radio' id='midazolamId' name='ucinkovina' value='midazolam'>
  <label for='midazolamId'>midazolam</label><br>
   <input type='radio' id='medetomidinId' name='ucinkovina' value='medetomidin'>
  <label for='medetomidinId'>medetomidin</label><br>
-<input id='tezaPremedikacijaId' type = 'text' name='teza' value=''>
+<input id='tezaPremedikacijaId' type = 'text' name='teza' value='1'>
 <input type='submit' value='izberi' name='submit'>
 </form>
-<script>
-let teza;
-function otroskaFunction(){
-	    teza = document. getElementById('teza').value;
-		alert(teza);
-		document.getElementById('tezaPremedikacijaId').value=teza;
-}
-</script>
-
 ";
-
 ?>
+<script>
+otroskaFunction();
+</script>
