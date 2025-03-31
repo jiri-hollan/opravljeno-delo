a26674c8fc0f95a4d7c635bdfb41347ea27ec2c8
Jiri Hollan <jiri.hollan@gmail.com>
2025-03-31 16:24:43 +0200

    popravljen medetomidin

--- pregled/sabloni/formaOtroskaPremedikacija.php
+++ pregled/sabloni/formaOtroskaPremedikacija.php
@@ -13,8 +13,8 @@ echo"
 <form id='otroskaFrm' name='otroskaForma' method='post' action='otroskaPremedikacija.php' autocomplete='off'> 
   <input type='radio' id='midazolamId' name='ucinkovina' value='midazolam'>
  <label for='midazolamId'>midazolam</label><br>
-  <input type='radio' id='medetomidinId' name='ucinkovina' value='medetomidin'>
- <label for='medetomidinId'>medetomidin</label><br>
+  <input type='radio' id='deksmedetomidinId' name='ucinkovina' value='deksmedetomidin'>
+ <label for='deksmedetomidinId'>deksmedetomidin</label><br>
 <input id='tezaPremedikacijaId' type = 'text' name='teza' value='1'>
 <input type='submit' value='izberi' name='submit'>
 </form>
