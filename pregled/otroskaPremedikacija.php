118f07d26bdd5435be8cd08c7d064dccacb3974d
Jiří Hollan <jiri.hollan@gmail.com>
2025-03-31 14:53:55 +0200

    mal igre s kodo

--- pregled/otroskaPremedikacija.php
+++ pregled/otroskaPremedikacija.php
@@ -5,11 +5,11 @@ require_once 'sabloni/formaOtroskaPremedikacija.php';
 if(isset($_POST['ucinkovina'])&&isset($_POST['teza'])){
 	$ucinkovina=$_POST['ucinkovina'];
 	$teza=$_POST['teza'];
-		echo 'Teža= '.$teza;
+	/*	echo 'Teža= '.$teza;
 		echo'<br>';
-		echo 'Učinkovina= '.$ucinkovina;		
+		echo 'Učinkovina= '.$ucinkovina;	*/	
     $prem=new Premedikace($ucinkovina, $teza);
-	echo $prem->get_name();
+	echo'<br>'. $prem->get_name();
 }//else{echo'Ni določena učinkovina ali teža';}
 
 class Premedikace {
@@ -18,6 +18,9 @@ class Premedikace {
 	public Function __construct($ucinkovina, $teza){	
 	$this->ucinkovina = $ucinkovina;
 	$this->teza = $teza;
+	echo 'Teža= '.$this->teza ;
+	echo'<br>';
+	echo 'Učinkovina= '.$this->ucinkovina;	
 	}
 	function get_name() {
     return $this->ucinkovina;
