9eb7ab35b88a81bade20b29daa3331638d574a22
Jiří Hollan <jiri.hollan@gmail.com>
2025-04-01 13:35:16 +0200

    dopolnjevanje CSS

--- otroska/sabloni/formaOtroskaPremedikacija.php
+++ otroska/sabloni/formaOtroskaPremedikacija.php
@@ -4,16 +4,16 @@ function otroskaFunction(){
 		//alert(teza);
 		//document.getElementById('tezaPremedikacijaId').setAttribute("value",teza);
 		document.getElementById('tezaPremedikacijaId').value=teza;		
-}
+}  
 </script>
 <div id='doziranje'  class='modal'>
-<form id='otroskaFrm' class="modal-content" name='otroskaForma' method='post' action='otroskaPremedikacija.php' autocomplete='off'> 
-  <input type='radio' id='midazolamId' name='ucinkovina' value='midazolam'>
+<form id='otroskaFrm' class="modal-content animate" name='otroskaForma' method='post' action='otroskaPremedikacija.php' autocomplete='off'> 
+  <input type='radio' id='midazolamId' class='ucinkovina' name='ucinkovina' value='midazolam'>
  <label for='midazolamId'>midazolam</label><br>
-  <input type='radio' id='deksmedetomidinId' name='ucinkovina' value='deksmedetomidin'>
+  <input type='radio' id='deksmedetomidinId' class='ucinkovina' name='ucinkovina' value='deksmedetomidin'>
  <label for='deksmedetomidinId'>deksmedetomidin</label><br>
 <input id='tezaPremedikacijaId' type = 'text' name='teza' value='1'>
-<input type='submit' value='izberi' name='submit'>
+<button type='submit'  class="signupbtn" name='submit'>potrdi</button>
 </form>
 </div>
 <script>
