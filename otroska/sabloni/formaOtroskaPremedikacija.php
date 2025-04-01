617926b579696a3e70e249c03810628daabe8800
Jiri Hollan <jiri.hollan@gmail.com>
2025-04-01 18:17:59 +0200

    vidi se input teza

--- otroska/sabloni/formaOtroskaPremedikacija.php
+++ otroska/sabloni/formaOtroskaPremedikacija.php
@@ -5,7 +5,8 @@
  <label for='midazolamId'>midazolam</label><br>
   <input type='radio' id='deksmedetomidinId' class='ucinkovina' name='ucinkovina' value='deksmedetomidin'>
  <label for='deksmedetomidinId'>deksmedetomidin</label><br>
-<input id='tezaPremedikacijaId' class='udaje' type = 'text' name='teza' value='1'>
+<label for='tezaPremedikacijaId'><b>Teža</b></label><br>
+<input id='tezaPremedikacijaId' class='udaje' type = 'text' name='teza' value='1' readonly>
 <button type='submit'  class="potrdiBtn" name='submit'>potrdi</button>
 </form>
 </div>
