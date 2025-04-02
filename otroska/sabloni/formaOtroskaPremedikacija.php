e965490b0a5c8a17c6a35b625e9d81a7e655066e
Jiri Hollan <jiri.hollan@gmail.com>
2025-04-02 15:47:32 +0200

    lepotna ureditev kode

--- otroska/sabloni/formaOtroskaPremedikacija.php
+++ otroska/sabloni/formaOtroskaPremedikacija.php
@@ -1,19 +1,18 @@
 
 <div id='doziranje'  class='modal'>
 <form id='otroskaFrm' class="modal-content animate" name='otroskaForma' method='post' action='bolnik.php' autocomplete='off'> 
-    <div class="container">
+  <div class="container">
       <h1>Otroška premedikacija</h1>
-  <input type='radio' id='midazolamId' class='ucinkovina' name='ucinkovina' value='midazolam'>
- <label for='midazolamId'>midazolam</label><br>
-  <input type='radio' id='deksmedetomidinId' class='ucinkovina' name='ucinkovina' value='deksmedetomidin'>
- <label for='deksmedetomidinId'>deksmedetomidin</label><br>
-<label for='tezaPremedikacijaId'><b>Teža</b></label><br>
-<input id='tezaPremedikacijaId' class='udaje' type = 'text' name='teza' value='1' readonly>
-<button type='submit'  class="potrdiBtn" name='submit'>potrdi</button>
- <div class="clearfix">
+      <input type='radio' id='midazolamId' class='ucinkovina' name='ucinkovina' value='midazolam'>
+	   <label for='midazolamId'>midazolam</label><br>
+      <input type='radio' id='deksmedetomidinId' class='ucinkovina' name='ucinkovina' value='deksmedetomidin'>
+       <label for='deksmedetomidinId'>deksmedetomidin</label><br>
+      <label for='tezaPremedikacijaId'><b>Teža</b></label><br>
+      <input id='tezaPremedikacijaId' class='udaje' type = 'text' name='teza' value='1' readonly>
+      <button type='submit'  class="potrdiBtn" name='submit'>potrdi</button>
+      <div class="clearfix">
         <button type="button" onclick="document.getElementById('doziranje').style.display='none'" class="cancelbtn">zapri</button>
       </div>
-
-    </div>
+  </div><!--container-->
 </form>
 </div>
