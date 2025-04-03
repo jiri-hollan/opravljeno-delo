b77eeb1f8892c48c6a565f69f4eb0a6700ff4245
Jiri Hollan <jiri.hollan@gmail.com>
2025-04-03 10:35:11 +0200

    lepotna ureitev kode

--- otroska/otroskaPremedikacija.php
+++ otroska/otroskaPremedikacija.php
@@ -35,20 +35,20 @@ class VyberTezo {
 public $tabulka;
  function __construct( $teza, $poradi) {
 	    $tabulka="premedikacijaTbl";
-		//$stolpci=["id", "teza", "midazolamDoza", "midazolamKoncentracija", "midazolamNavodila", "dexmedetomidinDoza", "dexmedetomidinKoncentracija", "dexmedetomidinNavodila", "ketaminDoza", "ketaminKoncentracija", "ketaminNavodila"];
+//$stolpci=["id", "teza", "midazolamDoza", "midazolamKoncentracija", "midazolamNavodila", "dexmedetomidinDoza", "dexmedetomidinKoncentracija", "dexmedetomidinNavodila", "ketaminDoza", "ketaminKoncentracija", "ketaminNavodila"];
 		$stolpci=["id", "teza", "midazolamDoza", "midazolamKoncentracija", "midazolamNavodila"];
 		$podminka = [];	
 		$this->teza = $teza;
 	    $podminka["teza<="] = $this->teza;
-   /* stolpci se morajo ujemati z nadpisi stlpcev v "if(count)" linija 105*/
-   //$stolpci=["id", "teza", "midazolamDoza", "midazolamKoncentracija", "midazolamNavodila", "dexmedetomidinDoza", "dexmedetomidinKoncentracija", "dexmedetomidinNavodila", "ketaminDoza", "ketaminKoncentracija", "ketaminNavodila"];
-   //echo"<tr class='glavaTable'><th>id</th><th>teza</th><th>midazolamDoza</th><th>midazolamKoncentracija</th><th>midazolamNavodila</th><th>dexmedetomidinDoza</th><th>dexmedetomidinKoncentracija</th><th>dexmedetomidinNavodila</th><th>ketaminDoza</th><th>ketaminKoncentracija</th><th>ketaminNavodila</th></tr>";
+/* stolpci se morajo ujemati z nadpisi stlpcev v "if(count)" linija 105*/
+//$stolpci=["id", "teza", "midazolamDoza", "midazolamKoncentracija", "midazolamNavodila", "dexmedetomidinDoza", "dexmedetomidinKoncentracija", "dexmedetomidinNavodila", "ketaminDoza", "ketaminKoncentracija", "ketaminNavodila"];
+//echo"<tr class='glavaTable'><th>id</th><th>teza</th><th>midazolamDoza</th><th>midazolamKoncentracija</th><th>midazolamNavodila</th><th>dexmedetomidinDoza</th><th>dexmedetomidinKoncentracija</th><th>dexmedetomidinNavodila</th><th>ketaminDoza</th><th>ketaminKoncentracija</th><th>ketaminNavodila</th></tr>";
    $vyber = new database();
    $vybrano=$vyber->otroska($tabulka,$stolpci, $podminka, $poradi );
 //echo $vybrano[1];
 //echo var_dump($vybrano);
- //  echo "<br>";
- //echo count($vybrano);
+//  echo "<br>";
+//echo count($vybrano);
 //$dolzina=count($vybrano);
 //echo $vybrano[1];
 //echo "<br>";
