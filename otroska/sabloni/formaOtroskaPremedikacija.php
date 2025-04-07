bece80a9bb93719d7034a392e1ce39066d4f863e
Jiri Hollan <jiri.hollan@gmail.com>
2025-04-07 18:13:55 +0200

    buttoncancel

--- otroska/sabloni/formaOtroskaPremedikacija.php
+++ otroska/sabloni/formaOtroskaPremedikacija.php
@@ -1,7 +1,8 @@
 
 <div id='doziranje'  class='modal'>
 <form id='otroskaFrm' class="modal-content animate" name='otroskaForma' method='post' action='bolnik.php' autocomplete='off'> 
-  <div class="container">
+  <div class="container">        
+	  <button type="button" onclick="document.getElementById('doziranje').style.display='none'"class="close" title="zapri">&times;</button>
       <h3>Otroška premedikacija</h3>
       <input  onclick='document.getElementById("otroskaFrm").submit();' type='radio' id='midazolamId' class='ucinkovina' name='ucinkovina' value='midazolam'>
 	   <label for='midazolamId'>midazolam</label><br>
@@ -12,7 +13,7 @@
 	  <br><br><br>
       <!--<button type='submit'  class="potrdiBtn" name='submit'>potrdi</button>-->
       <div class="clearfix">
-        <button type="button" onclick="document.getElementById('doziranje').style.display='none'" class="cancelbtn">zapri</button>
+     <!-- <button type="button" onclick="document.getElementById('doziranje').style.display='none'" class="cancelbtn">zapri</button>-->
       </div>
   </div><!--container-->
 </form>
