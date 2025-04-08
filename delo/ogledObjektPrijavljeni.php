966bc6925a84d655e98169af762f3b952478946a
Jiri Hollan <jiri.hollan@gmail.com>
2025-04-08 20:11:52 +0200

    Auto stash before checking out "HEAD"
    potrebno je preveriti

--- delo/ogledObjektPrijavljeni.php
+++ delo/ogledObjektPrijavljeni.php
@@ -37,8 +37,8 @@ if (isset($_REQUEST["akce"])) {
   //var_dump($akce);
   echo strtoupper($akce) .': ';
   echo '<b>dne: </b>'.($datumOpravila) .'<br>';
- //echo "tabulka je: ".$tabulka;
-  new $akce($stevilkaUporabnika, $datumOpravila, $tabulka);
+ echo "tabulka je: ".$tabulka;
+  new $akce($stevilkaZdravnika, $datumOpravila, $tabulka);
 
 	  
 }//od if akce
